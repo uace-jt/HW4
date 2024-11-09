1c9898bd692a3516b761621ef9d78b4292fc99b5
uace-jt <wilson950403@gmail.com>
2024-11-09 17:11:59 +0800

    Add files via upload

---
+++ 6.11.cpp
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j;
+    int swapped;
+
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0; 
+
+        
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+               
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                swapped = 1; 
+            }
+        }
+
+        
+        if (swapped == 0) {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("原始數組: ");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("排序後的數組: ");
+    printArray(arr, n);
+
+    system("pause");
+    return 0;
+}
